ed13a0da75ccb100b4475a14b35afbfc512739d4
Aarsh <aarsh@1x-nat-vl930-172-30-101-181.wireless.umass.edu>
2018-08-06 11:20:49 -0400

    Updated the titlebar color for iterm2 in the fish config file

--- config.fish
+++ config.fish
@@ -2,9 +2,9 @@ export DB_USER="db_user"
 export DB_PASS="db_pass"
 
 
-echo -e "\033]6;1;bg;red;brightness;40\a"
-echo -e "\033]6;1;bg;green;brightness;44\a"
-echo -e "\033]6;1;bg;blue;brightness;52\a"
+echo -e "\033]6;1;bg;red;brightness;23\a"
+echo -e "\033]6;1;bg;green;brightness;23\a"
+echo -e "\033]6;1;bg;blue;brightness;23\a"
 
 export PATH="/Users/aarsh/anaconda2/bin:$PATH"
 export PATH="/opt/bin:$PATH"

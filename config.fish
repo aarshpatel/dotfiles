22f59a619de2d68ce4beff0d8d1808cdb99575b3
Aarsh <aarsh@1x-nat-vl930-172-30-101-181.wireless.umass.edu>
2018-08-06 12:04:53 -0400

    Updated the fish configuration with comments

--- config.fish
+++ config.fish
@@ -1,12 +1,13 @@
-export DB_USER="db_user"
-export DB_PASS="db_pass"
-
+# Titlebar Color Configuration
 
 echo -e "\033]6;1;bg;red;brightness;23\a"
 echo -e "\033]6;1;bg;green;brightness;23\a"
 echo -e "\033]6;1;bg;blue;brightness;23\a"
 
+# Python Anaconda Location
 export PATH="/Users/aarsh/anaconda2/bin:$PATH"
 export PATH="/opt/bin:$PATH"
+
+# Editor Configuration
 export EDITOR='vim'
 
